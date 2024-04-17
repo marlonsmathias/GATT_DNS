94456de2d8d6cbf36ee4968de777a2a697c6dd93
marlon <marlonsmathias@gmail.com>
2024-04-17 14:16:04 -0300

    Added energy normalization in turbulence generator

--- source/disturbances/turbulenceGenerator.F90
+++ source/disturbances/turbulenceGenerator.F90
@@ -1,5 +1,7 @@
     subroutine turbulenceGenerator(nx,ny,nz,x,y,z,u,v,w,du,dv,dw,epsilon)
 
+    use mpi
+
     implicit none
     
     integer, intent(in) :: nx, ny, nz
@@ -9,9 +11,21 @@
     real*8, dimension(nx,ny,nz),intent(in) :: u, v, w
     real*8, dimension(nx,ny,nz),intent(inout) :: du, dv, dw
     real*8, intent(in) :: epsilon
+
+    real*8 :: E_local, E
+    integer :: N_local, N
+    integer :: ierror
     
-    du = du + epsilon*u
-    dv = dv + epsilon*v
-    dw = dw + epsilon*w
+    N_local = nx * ny * nz
+    E_local = sum(U*U + V*V + W*W)
+
+    call MPI_ALLREDUCE(E_local, E, 1, MPI_REAL8, MPI_SUM, MPI_COMM_WORLD, ierror)
+    call MPI_ALLREDUCE(N_local, N, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD, ierror)
+
+    E = E/N
+
+    du = du + 0.5*epsilon/E*u
+    dv = dv + 0.5*epsilon/E*v
+    dw = dw + 0.5*epsilon/E*w
     
     end subroutine
